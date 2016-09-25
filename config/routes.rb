ec081faa43a6da16c6e9388e5b25d17e73275d0e
KerryAlsace <kerrydougherty@gmail.com>
2016-09-25 19:13:56 -0400

    revise navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/login' => 'sessions#new'
-  post '/sessions' => 'sessions#create'
+  post '/login' => 'sessions#create'
   get '/logout' => 'sessions#destroy'
+
+  # get '/login' => 'sessions#new'
+  # post '/sessions' => 'sessions#create'
+  # get '/logout' => 'sessions#destroy'
 
   # get '/login' => "sessions#new"
   # post '/login' => "sessions#create"
@@ -17,3 +21,24 @@ end
   # resources :users do
   #   resources :friends
   # end
+
+#      login GET    /login(.:format)           sessions#new
+#            POST   /login(.:format)           sessions#create
+#     logout GET    /logout(.:format)          sessions#destroy
+#      users GET    /users(.:format)           users#index
+#            POST   /users(.:format)           users#create
+#   new_user GET    /users/new(.:format)       users#new
+#  edit_user GET    /users/:id/edit(.:format)  users#edit
+#       user GET    /users/:id(.:format)       users#show
+#            PATCH  /users/:id(.:format)       users#update
+#            PUT    /users/:id(.:format)       users#update
+#            DELETE /users/:id(.:format)       users#destroy
+#     places GET    /places(.:format)          places#index
+#            POST   /places(.:format)          places#create
+#  new_place GET    /places/new(.:format)      places#new
+# edit_place GET    /places/:id/edit(.:format) places#edit
+#      place GET    /places/:id(.:format)      places#show
+#            PATCH  /places/:id(.:format)      places#update
+#            PUT    /places/:id(.:format)      places#update
+#            DELETE /places/:id(.:format)      places#destroy
+#       root GET    /                          places#index
