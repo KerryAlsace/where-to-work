7f8a74d08a46bd439aaf99832a772489ab985424
KerryAlsace <kerrydougherty@gmail.com>
2016-09-25 17:51:10 -0400

    revise sessions view

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,8 +4,8 @@ class SessionsController < ApplicationController
   end
 
   def create
-    @user = User.find_by(username: params[:username])
-    if @user && @user.authenticate(params[:password])
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
       session[:user_id] = user.id
       flash[:notice] = "Log in successful"
 
@@ -25,3 +25,17 @@ class SessionsController < ApplicationController
   end
 
 end
+
+# def create
+#     user = User.find_by(name: params[:user][:name])
+#     user = user.try(:authenticate, params[:user][:password])
+#     return redirect_to(controller: 'sessions', action: 'new') unless user
+#     session[:user_id] = user.id
+#     @user = user
+#     redirect_to(controller: 'welcome', action: 'home')
+#   end
+
+#   def destroy
+#     session.delete :user_id
+#     redirect_to(controller: 'sessions', action: 'new')
+#   end
