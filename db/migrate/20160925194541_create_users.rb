7895c1aa0706a40e5728626847675a33d782babe
KerryAlsace <kerrydougherty@gmail.com>
2016-09-28 16:34:42 -0400

    set user default role to 'user'

--- db/migrate/20160925194541_create_users.rb
+++ db/migrate/20160925194541_create_users.rb
@@ -3,7 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.0]
     create_table :users do |t|
       t.string :username
       t.string :password_digest
-      t.integer :role
+      t.integer :role, default: "user"
       t.string :provider
       t.string :uid
       t.string :name
