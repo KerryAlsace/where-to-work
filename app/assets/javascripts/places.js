2be99bc653c431bc6c98dd01aab7f4e7b592b988
KerryAlsace <kerrydougherty@gmail.com>
2016-11-07 21:26:03 -0500

    remove debugger

--- app/assets/javascripts/places.js
+++ app/assets/javascripts/places.js
@@ -2,17 +2,14 @@
 $(function() {
   // When a user clicks to load comments
   $("a.load_comments").on("click", function(e){
+    
     // Send AJAX get request and if successful:
     $.get(this.href).success(function(json){
-
       var $div = $("div.comments")
       $div.html("")
 
       // Add comments to DOM
       json.forEach(function(comment){
-
-        debugger
-
         $div.append("<p>" + comment.body + "</p>");
       });
     });
