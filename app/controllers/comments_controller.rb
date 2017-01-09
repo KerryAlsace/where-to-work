45003100b2de691b13bfa80c0b8fb8ba67be660f
KerryAlsace <kerrydougherty@gmail.com>
2017-01-09 12:23:31 -0500

    again edit seed file to now have correct comments and shared places

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -17,6 +17,14 @@ class CommentsController < ApplicationController
   end
 
   private
+    def define_place
+      if params[:place_id]
+        @place = Place.find(params[:place_id])
+      else
+        @place = Place.find(params[:id])
+      end
+    end
+
     def comments_params
       params.require(:comment).permit(:body)
     end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,55 +5,26 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
 
 random_boolean = [true, false].sample
 
 10.times do |i|
   id = i + 1
+  other_id = id + 1
   neighborhood = Neighborhood.create(name: "Neighborhood #{id}")
   u = User.create(username: "user#{id}", password: "test1234")
 
   5.times do |n|
-    u.places.create(name: "Place #{i + n}", neighborhood: neighborhood, wifi: "#{random_boolean}", wifi_quality: "#{rand(5)}", public_restroom: "#{random_boolean}", restroom_cleanliness: "#{rand(5)}", costs_money: "#{random_boolean}", creator_id: u.id, address_attributes: { street1: "#{rand(5)} Main st.", city: "New York", state: "NY", zipcode: "1000#{i}"})
+    p = u.places.create(name: "#{Faker::LordOfTheRings.unique.location}", neighborhood: neighborhood, wifi: "#{random_boolean}", wifi_quality: "#{rand(5)}", public_restroom: "#{random_boolean}", restroom_cleanliness: "#{rand(5)}", costs_money: "#{random_boolean}", creator_id: u.id, address_attributes: { street1: "#{rand(5)} Main st.", city: "New York", state: "NY", zipcode: "1000#{i}"})
+    SharedPlace.create(place_id: p.id, friend_id: other_id)
+    3.times do |r|
+      c = Comment.create(place_id: p.id, body: "#{Faker::ChuckNorris.fact}")
+      p.comments << c
+    end
   end
-end
-
-5.times do |i|
-  id = i + 1
-  p = Place.find(id)
-  c = Comment.create(place_id: id, body: "Place #{id} is great")
-  p.comments << c
-end
-
-5.times do |i|
-  id = i + 1
-  other_id = id + 1
-  SharedPlace.create(place_id: id, friend_id: other_id)
 end
 
 User.first.admin!
 
 User.last.admin!
-
-# 7.times do
-#   neighborhood = Neighborhood.create(name: Faker::GameOfThrones.city)
-#   password = "test1234"
-#   u = User.new(password: password, password_confirmation: password, username: Faker::GameOfThrones.character )
-#   u.save!
-#   p = u.places.new(name: Faker::Space.star, neighborhood: neighborhood, address: Faker::Address.street_address, wifi: Faker::Boolean.boolean, wifi_quality: Faker::Number.between(1, 4), public_restroom: Faker::Boolean.boolean, restroom_cleanliness: Faker::Number.between(1, 4), costs_money: Faker::Boolean.boolean, creator_id: u.id)
-#   p.save!
-# end
-
-# 5.times do |i|
-#   id = i + 1
-#   other_id = id + 1
-#   s = SharedPlace.new(place_id: id, friend_id: other_id, comment: Faker::ChuckNorris.fact)
-#   s.save!
-# end
-
-# u = User.first
-# u.role = "admin"
-# u.save!
-# u = User.last
-# u.role = "admin"
-# u.save!
